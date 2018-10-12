4aea32af89fd855faf40da3e7ae39e1490a7e2aa
Muhammad Azhar Rasyad <muhazharrasyad@gmail.com>
2018-10-12 20:44:41 +0700

    Add files via upload

---
+++ Semester 3/Pemrograman Berorientasi Objek/Teori/Tugas/Teori 1/HelloPrinter.java
@@ -0,0 +1,17 @@
+public class HelloPrinter
+{
+  public static void main(String [] args)
+  {
+    System.out.println("........................................");
+    System.out.println("|  ..................................  |");
+    System.out.println("|  |   _____    _______    ______   |  |");
+    System.out.println("|  |  |  _  |  |   _   |  |  __  |  |  |");
+    System.out.println("|  |  | |_| |  |  |_| _|  | |  | |  |  |");
+    System.out.println("|  |  |   __|  |   _ |_   | |  | |  |  |");
+    System.out.println("|  |  |  |     |  |_|  |  | |__| |  |  |");
+    System.out.println("|  |  |__|     |_______|  |______|  |  |");
+    System.out.println("|  |                                |  |");
+    System.out.println("|  ..................................  |");
+    System.out.println("........................................");
+  }
+}
