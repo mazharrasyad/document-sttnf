5d6c2d1979271be4a9088877fca8b337634dd143
Muhammad Azhar Rasyad <muhazharrasyad@gmail.com>
2018-10-12 20:50:55 +0700

    Add files via upload

---
+++ Semester 3/Pemrograman Berorientasi Objek/Teori/Tugas/Quiz 1/No 2/DaftarBiodata.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class DaftarBiodata
+{
+  public static void main(String [] args)
+  {
+    Scanner in = new Scanner(System.in);
+
+    System.out.println("========== Biodata ==========");
+    Biodata mahasiswa1 = new Biodata("Azhar", "0110217029", "Cibinong", "TI", 80);
+    System.out.println("=============================");
+  }
+}
