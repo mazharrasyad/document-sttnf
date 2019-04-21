58442a018c594f395edaf3fed1a876f74c63c86c
Muhammad Azhar Rasyad <muhazharrasyad@gmail.com>
2019-04-21 22:59:17 +0700

    Update 4.1 Akar Persamaan Non Linier.py

--- Semester 4/Analisis Numerik/Tugas/4.1 Akar Persamaan Non Linier.py
+++ Semester 4/Analisis Numerik/Tugas/4.1 Akar Persamaan Non Linier.py
@@ -1,5 +1,5 @@
-a = 0.7
-b = 1
+a = -1
+b = 7
 c = 0
 fa = 0
 fb = 0
@@ -7,14 +7,17 @@ fc = 0
 i = 0
 
 def fx(x) :
-    return (16 * (x ** 3)) - (22 * x) + 9
+    return (x ** 2) - 4
 
 print("Iterasi\ta\tf(a)\tb\tf(b)\tc\tf(c)")
 
-while True :
-    c = a + ((b - a) / 2)
+while True :    
     fa = fx(a)
     fb = fx(b)
+    # Metode Bagi Dua
+    c = a + ((b - a) / 2)
+    # Metode Posisi Palsu
+    # c = b - ((fb / (fb - fa)) * (b - a))
     fc = fx(c)
 
     print("%d\t%0.5f\t%0.5f\t%0.5f\t%0.5f\t%0.5f\t%0.5f" %(i,a,fa,b,fb,c,fc))
